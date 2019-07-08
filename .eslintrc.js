7369a661e0e9f366e683b01cb63986e3023036a1
Warley Gabriel da Paixão <wgabrieldapaixao@gmail.com>
2019-07-08 19:37:32 -0300

    Update `.eslintrc`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,25 @@
-const config = {
-    "extends": "airbnb-base",
-    "parser": "babel-eslint"
+module.exports = {
+    rules: {
+        'prefer-destructuring': 'warn',
+        'class-methods-use-this': 'off',
+        'global-require': 'off',
+        'func-names': 'off',
+        'import/prefer-default-export': 'off',
+        'no-magic-numbers': ['error', {
+            ignore: [0, 1, 2, 1000],
+            ignoreArrayIndexes: true,
+            enforceConst: true,
+        }],
+    },
+    globals: {
+        browser: true,
+        $: true,
+        $$: true,
+    },
+    env: {
+        mocha: true,
+    },
+    extends: [
+        'airbnb-base',
+    ]
 };
-
-config.rules = {
-    ...config.rules,
-    'prefer-destructuring': 'warn',
-    'class-methods-use-this': 'off',
-    'global-require': 'off',
-    'func-names': 'off',
-    'import/prefer-default-export': 'off',
-    'no-magic-numbers': ['error', {
-        ignore: [0, 1, 2, 1000],
-        ignoreArrayIndexes: true,
-        enforceConst: true,
-    }],
-};
-
-config.globals = {
-    ...config.globals,
-    browser: true,
-    $: true,
-};
-
-config.env = {
-    ...config.env,
-    mocha: true,
-};
-
-module.exports = config;
